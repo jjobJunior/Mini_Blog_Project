bd05803d32e873ea99fef7f399cd1462f054a4d1
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-05-10 11:31:36 -0300

    Estruturando a pagina Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,37 @@
+import { useNavigate, Link } from "react-router-dom";
 import styles from "./Home.module.css";
+import { useState } from "react";
 
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className={styles}>
-      <h1>Pag. Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.no_posts}>
+            <p>Não foram encontrados sposts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
